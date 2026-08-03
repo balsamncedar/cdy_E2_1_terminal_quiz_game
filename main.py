2ba2f5be050585abf98dcc71da031c8a54f9cf68
balsamncedar5 <balsamncedar5@gmail.com>
2026-08-03 19:45:44 +0900

    feat: main.py (키보드 예외처리) + pr_00_list 기본 문법 추가 리스트 작성(향후예정)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # [실행파일] 프로그램 시작점, 메뉴 루프 및 사용자 입력 수집 
 
 
+# display_menu() : 메뉴 선택 관련 함수
 # UI 관련 너비 매직넘버 방지용 변수 전역 선언 WIDTH (40으로 고정, 혹시 다른데서도 사용할까봐 전역처리 )
 WIDTH = 40
 
@@ -18,22 +19,47 @@ def display_menu():
     # print("선택 :        ", end="")
 
 
-def main():
-    display_menu()
-    choice= input("1 ~ 5 중 메뉴선택(이외 입력시 다시 선택) : ").strip()
+# handle_exit() : 키보드 인터럽트 Ctrl+C 공통 에러 처리 함수 , 나중에 사용할지도 
+def handle_exit(save_callback=None):
+    """Ctrl+ C 눌렀을 때 처리하는 공통함수"""
+    print("⚠️  강제 종료 신호 감지 ")
 
-    if choice == "1":
-        print("퀴즈 풀기 선택")
-    elif choice == "2":
-        print("퀴즈 추가 선택")
-    elif choice == "3":
-        print("퀴즈 추가 선택")
-    elif choice == "4":
-        print("점수 확인 선택")
-    elif choice == "5":
-        print("프로그램 종료 선택 ")
-    else:
-        print("⚠️  잘못된 입력입니다. 1 ~ 5 번 중 선택해주세요. ")
+    if save_callback:
+        print("💾 현재 진행 상황을 안전하게 저장 중입니다..")
+        save_callback()
+
+    print(" -- 적당한 굿바이멘트 ")
+    exit(0)
+
+
+
+
+def main():
+    try:
+        while True:
+            display_menu()
+            choice= input("1 ~ 5 중 메뉴선택(이외 입력시 다시 선택) : ").strip()
+
+            if choice == "1":
+                print("퀴즈 풀기 선택")
+                break;
+            elif choice == "2":
+                print("퀴즈 추가 선택")
+                break;
+            elif choice == "3":
+                print("퀴즈 추가 선택")
+                break;
+            elif choice == "4":
+                print("점수 확인 선택")
+                break;
+            elif choice == "5":
+                print("프로그램 종료 선택 ")
+                break;
+            else:
+                print("⚠️  잘못된 입력입니다. 1 ~ 5 번 중 선택해주세요. ")
+
+    except KeyboardInterrupt:
+        print("\nCtrl + C 입력, 메인 메뉴 프로그램 종료. ")
 
 if __name__ == "__main__":
     main()
--- practice/pr_00_list.py
+++ practice/pr_00_list.py
@@ -12,7 +12,9 @@ pr_03_path.py
 추가해야할 항목
 # - 변수선언 -> 전역변수, 매직넘버 등등  
 # - input  -> 메뉴작성후 선택 받을때 input 
-# - while 문  -> main.py 메뉴 작성이후에 돌릴때 기본 실행 때 사용
+# - while 문  / break -> main.py 메뉴 작성이후에 돌릴때 기본 실행 때 사용
 # - 기본 내장 함수 -> strip
 # - if ~ elif  ~ else 구문
+# - try ~ except 구문
+# - callback 함수 
 """
