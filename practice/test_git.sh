694f7f20e6bd6b025e787094cf95ee6a2bfa54a2
balsamncedar5 <balsamncedar5@gmail.com>
2026-08-04 08:45:46 +0900

    test: 깃 브랜치 생성후 머지하고 브랜치 삭제하는 과정 기록 스크립트 작성 및 연습

---
+++ practice/test_git.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+REPO_ROOT=$(git rev-parse --show-toplevel)
+
+echo "===== 1. practice-branch 생성 및 이동 (swtich : 이동, -c : create / 브랜치 생성) =====
+git switch -c practice
+
+# 브랜치 생성 이전 방식(git branch, checkout)
+# $ git branch feature/quiz-ui
+# $ git checkout feature/quiz-ui
+
+
+echo "===== 2. 테스트용 파일 생성 및 커밋 ==="
+mkdir -p "$REPO_ROOT/practice
+echo "Git branch test contenct" > practice_note.text
+
+# 깃 추가 및 커밋
+git add "$REPO_ROOT/practice/practice_note.txt"
+git commit -m "test: add practice note in practice folder"
+
+echo "========= 3. main 브랜치로 복귀 ====="
+git switch main
+
+echo "===== 4. practice 브랜치 병합 (merge) ===="
+git merge practice
+
+echo "=== 5. 사용 완료된 practice 브랜치 삭제 ==="
+git branch -d practice
+
+echo "=== 모든 깃 브랜치 테스트 과정이 프로젝트 루트 기준으로 완벽하게 완료 됌. ==="
+
+
+# 사용 시
+# chomod +x test_git.sh
+# ./test_git.sh
